4b3165f1765c57fd20b09cc4b3dd24ad83e18db6
nxc244 <nd54781@gmail.com>
2026-07-25 01:07:34 -0500

    correcion img

--- catalogo_musical/urls.py
+++ catalogo_musical/urls.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static # ¡Faltaba importar esta línea!
 
 urlpatterns = [
     # Este es el camino para el panel de administrador (ya viene por defecto)
     path('admin/', admin.site.urls),
+    
     # ¡Esta es la línea clave! 
     # Aquí le decimos a Django: "Oye, cuando alguien en internet escriba '/api/' 
     # en la barra de direcciones, envíalo a leer el mapa de rutas que 
     # nosotros creamos adentro de la carpeta 'api'."
     path('api/', include('api.urls')),
     
-    # NUEVO: Esta es la puerta para nuestro guardia de seguridad.
+    # Esta es la puerta para nuestro guardia de seguridad.
     # Cuando React necesite un Token, vendrá a esta dirección ('/o/').
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
+
+# Le dice a Django cómo mostrar las imágenes mientras están programando.
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
